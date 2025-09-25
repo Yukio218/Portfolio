d00ffe4d815199d709f501a6fdfc15fc389d7e74
Yukio <159587434+Yukio218@users.noreply.github.com>
2025-09-25 11:46:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+gsap.to(".pillar",
+{
+    y: "-120%",
+    duration: 1.2,
+    ease: "power4.inOut",
+    stagger: 0.2,
+    onComplete: () => document.querySelector(".loader").remove()
+});
+
+
+
+//Cherche
+const navLinks = document.querySelectorAll('.HoverMenu, .ActiveMenu');
+let currentActive = document.querySelector('.ActiveMenu');
+
+
+//Boucle dans chaque "a" qui est de class .HoverMenu et ou .ActiveMenu
+navLinks.forEach(link => 
+{
+  link.addEventListener('mouseenter', () => 
+  {
+    if (currentActive && currentActive !== link) 
+    {
+      currentActive.classList.remove('ActiveMenu');
+      currentActive.classList.add('HoverMenu');
+    }
+  });
+
+  link.addEventListener('mouseleave', () => 
+  {
+    if (currentActive && !currentActive.classList.contains('ActiveMenu')) 
+    {
+      currentActive.classList.remove('HoverMenu');
+      currentActive.classList.add('ActiveMenu');
+    }
+  });
+
+  link.addEventListener('click', () => 
+  {
+    if (currentActive) 
+    {
+      currentActive.classList.remove('ActiveMenu');
+      currentActive.classList.add('HoverMenu');
+    }
+
+    link.classList.remove('HoverMenu');
+    link.classList.add('ActiveMenu');
+    currentActive = link;
+  });
+});
